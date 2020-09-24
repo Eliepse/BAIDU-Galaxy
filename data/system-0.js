7b01234465479e97b5541e58c50a47140d2b175d
Élie Meignan <em@globesailor.fr>
2020-09-24 12:56:25 +0200

    Change data to js

---
+++ data/system-0.js
@@ -0,0 +1,10 @@
+import test from './system-0/dimension-test.json'
+import panda from './system-0/dimension-panda.json'
+
+export default {
+    "name" : "system-0",
+    "dimensions" : {
+        "test": test,
+        "panda": panda
+    }
+}
